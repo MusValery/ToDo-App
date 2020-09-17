b6b2d5b741bc6d5108a019042bede269df893808
Valeria Mustetsova <lera.mustetsova57@gmail.com>
2020-09-17 12:10:32 +0300

    New Todo

---
+++ script2.js
@@ -0,0 +1,78 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUl = document.getElementById('todos');
+const todos = JSON.parse(localStorage.getItem('todos'));
+const button = document.getElementById("btn");
+
+if (todos) {
+   todos.forEach((todo) => {
+      addTodo(todo)
+   });
+}
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   addTodo();
+});
+
+function addTodo(todo) {
+   let todoText = input.value;
+
+   if (todo) {
+      todoText = todo.text;
+   }
+
+   if (todoText) {
+      const elementLi = document.createElement('li');
+      if (todo && todo.completed) {
+         elementLi.classList.add('completed');
+      }
+
+      elementLi.innerText = todoText;
+
+      elementLi.addEventListener('click', () => {
+         elementLi.classList.toggle('completed');
+
+         updateLS();
+      });
+
+      elementLi.addEventListener('contextmenu', (e) => {
+         e.preventDefault();
+
+         elementLi.remove();
+
+         updateLS();
+      });
+
+      todosUl.appendChild(elementLi);
+
+      input.value = "";
+
+      updateLS();
+   }
+}
+
+function updateLS() {
+   const todosEl = document.querySelectorAll('li');
+
+   const todos = [];
+
+   todosEl.forEach((elementLi) => {
+      todos.push({
+         text: elementLi.innerText,
+         completed: elementLi.classList.contains('completed')
+      })
+   });
+
+   localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+
+button.addEventListener("click", clearAll);
+
+function clearAll() {
+   todosUl.innerHTML = '';
+   input.value = "";
+   updateLS();
+}
